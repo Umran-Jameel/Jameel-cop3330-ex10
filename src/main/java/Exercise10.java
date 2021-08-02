068457a8e29d61863a11648019bfe9d30cba2f2e
umranrazack02 <umranrazack02@outlook.com>
2021-08-02 00:11:12 -0400

    challanges

--- src/main/java/Exercise10.java
+++ src/main/java/Exercise10.java
@@ -10,31 +10,43 @@ public class Exercise10 {
     {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter the price of item 1: ");
-        String price1 = in.nextLine();
-        double p1 = Double.parseDouble(price1);
-        System.out.print("Enter the quantity of item 1: ");
-        String quantity1 = in.nextLine();
-        int q1 = Integer.parseInt(quantity1);
-        p1 *= q1;
+        ArrayList<Double> values = new ArrayList<>();
+        int counter = 1;
 
-        System.out.print("Enter the price of item 2: ");
-        String price2 = in.nextLine();
-        double p2 = Double.parseDouble(price2);
-        System.out.print("Enter the quantity of item 2: ");
-        String quantity2 = in.nextLine();
-        int q2 = Integer.parseInt(quantity2);
-        p2 *= q2;
+        // program changed for challenge, user can enter infinite amount of items, thoretically
+        while (true) {
+            System.out.printf("Enter the price of item %d (enter done if done): ", counter);
+            String input = in.nextLine();
+            double p = 0;
+            if (input.equals("done")) {
+                break;
+            }
+            else {
+                // ensuring the user enters numerical data (challenge)
+                try {
+                    p = (Double.parseDouble(input));
+                } catch (NumberFormatException e) {
+                    System.out.println("Please enter a numeric value or enter done");
+                    System.exit(0);
+                }
+            }
 
-        System.out.print("Enter the price of item 3: ");
-        String price3 = in.nextLine();
-        double p3 = Double.parseDouble(price3);
-        System.out.print("Enter the quantity of item 3: ");
-        String quantity3 = in.nextLine();
-        int q3 = Integer.parseInt(quantity3);
-        p3 *= q3;
-
-        double subtotal = p1 + p2 + p3;
+            System.out.printf("Enter the quantity of item %d (enter done if done): ", counter);
+            String inputQ = in.nextLine();
+            double q = 0;
+            try {
+                q = Double.parseDouble(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a numeric value or enter done");
+                System.exit(0);
+            }
+            values.add(p * q);
+            counter++;
+        }
+        double subtotal = 0;
+        for (int i = 0; i < values.size(); i++) {
+            subtotal += values.get(i);
+        }
         double tax = 0.055 * subtotal;
         double total = tax + subtotal;
 
